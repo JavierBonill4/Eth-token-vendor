b5f8466967c4df8836ce225d69d3e89f3075a5e7
Javier Bonilla <bjavier@umich.edu>
2025-12-24 23:54:25 -0500

    passing all 6 tests

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -31,19 +31,29 @@ contract Vendor is Ownable {
         ETH_funds += msg.value;
         yourToken.transfer(msg.sender, amount_transfer);
         emit BuyTokens(msg.sender, msg.value, amount_transfer);
-
     }
 
 
     // ToDo: create a withdraw() function that lets the owner withdraw ETH
     function withdraw() external {
-        require(msg.sender == owner_addr, "Only owner can withdraw ETH");
+        require(msg.sender == owner(), "Only owner can withdraw ETH");
         uint256 balance = ETH_funds;
         require(balance > 0, "No balance to withdraw");
 
         ETH_funds = 0;
-        payable(msg.sender).transfer(balance);
+        payable(owner()).transfer(balance);
     }
 
     // ToDo: create a sellTokens(uint256 _amount) function:
+
+    function sellTokens(uint256 _amount) external{
+        require(_amount > 0, "More than 0 tokens needed to sell");
+        uint256 allowance = yourToken.allowance(msg.sender, address(this));
+        require(allowance >= _amount, "Not enough allowance");
+        yourToken.transferFrom(msg.sender, address(this), _amount);
+        uint256 ethToReturn = _amount / tokensPerEth;
+        payable(msg.sender).transfer(ethToReturn);
+
+        ETH_funds -= ethToReturn;
+    }
 }
