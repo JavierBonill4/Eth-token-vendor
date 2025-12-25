8d4b337fc0581f59003d364507757c4aecae977e
Javier Bonilla <bjavier@umich.edu>
2025-12-24 21:19:44 -0500

    Withdraw only for vendor owner

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -9,9 +9,12 @@ contract Vendor is Ownable {
 
     YourToken public yourToken;
     uint256 public constant tokensPerEth = 100;
+    uint256 public ETH_funds = 0;
+    address owner_addr;
 
     constructor(address tokenAddress) Ownable(msg.sender) {
         yourToken = YourToken(tokenAddress);
+        owner_addr = msg.sender;
     }
 
     // ToDo: create a payable buyTokens() function:
@@ -25,7 +28,7 @@ contract Vendor is Ownable {
         require(msg.value > 0, "Requires some purchase greater than 0");
         uint256 amount_transfer = msg.value * tokensPerEth;
         require(yourToken.balanceOf(address(this)) >= amount_transfer, "Vendor has insufficient tokens");
-
+        ETH_funds += msg.value;
         yourToken.transfer(msg.sender, amount_transfer);
         emit BuyTokens(msg.sender, msg.value, amount_transfer);
 
@@ -33,6 +36,14 @@ contract Vendor is Ownable {
 
 
     // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() external {
+        require(msg.sender == owner_addr, "Only owner can withdraw ETH");
+        uint256 balance = ETH_funds;
+        require(balance > 0, "No balance to withdraw");
+
+        ETH_funds = 0;
+        payable(msg.sender).transfer(balance);
+    }
 
     // ToDo: create a sellTokens(uint256 _amount) function:
 }
